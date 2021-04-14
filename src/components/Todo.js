21f8b2422170d27276ea18bfd5d2c3a351164ecc
LongNguyenVu181 <longnguyenvu181@gmail.com>
2021-04-14 05:39:28 +0000

    Save Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,41 +1,30 @@
 import React, { useState } from 'react';
 
-/* 
-  【Todoのデータ構成】
-　・key：Todoを特定するID（String）
-　・text：Todoの内容（String）
-　・done：完了状態（Boolean true:完了済み,, false:未完了）
-*/
-
 /* コンポーネント */
 import TodoItem from './TodoItem';
 import Input from './Input';
 import Filter from './Filter';
 
 /* カスタムフック */
-import useStorage from '../hooks/storage';
+// import useStorage from '../hooks/storage';
+import useFbStorage from '../hooks/fbStorage';
 
 /* ライブラリ */
 import {getKey} from "../lib/util";
 
 function Todo() {
-  const [items, addItem,updateItem] = React.useState([
-      /* テストコード 開始 */
-    { key: getKey(), text: '日本語の宿題', done: false },
-    { key: getKey(), text: 'reactを勉強する', done: false },
-    { key: getKey(), text: '明日の準備をする', done: false },
-    /* テストコード 終了 */
-  ]);
+  // const [items, putItems, clearItems] = useStorage();
+  const [items, addItem, updateItem, clearItems] = useFbStorage();
   
   const [filter, setFilter] = React.useState('ALL');
-  
-    const displayItems = items.filter(item => {
+
+  const displayItems = items.filter(item => {
     if (filter === 'ALL') return true;
     if (filter === 'TODO') return !item.done;
     if (filter === 'DONE') return item.done;
   });
   
-    const handleCheck = checked => {
+  const handleCheck = checked => {
     // const newItems = items.map(item => {
     //   if (item.key === checked.key) {
     //     item.done = !item.done;
@@ -52,6 +41,7 @@ function Todo() {
   };
   
   const handleFilterChange = value => setFilter(value);
+
   return (
     <article class="panel is-danger">
       <div className="panel-heading">
